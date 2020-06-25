4880860a8b3ce81e5d811c53ecf68dc4e00a89b4
Claudiu Andries <61622980+WiDuAlK12@users.noreply.github.com>
2020-06-25 18:20:30 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using AutolocatorWPF.Views;
+
+
+namespace AutolocatorWPF
+{
+        /// <summary>
+        /// Interaction logic for App.xaml
+        /// </summary>
+        public partial class App : PrismApplication
+        {
+                protected override Window CreateShell()
+                {
+                        return Container.Resolve<MainPage>();
+                }
+
+                protected override void RegisterTypes(IContainerRegistry containerRegistry)
+                {
+                        containerRegistry.RegisterForNavigation<MainPage>("MainPage");
+
+                        containerRegistry.RegisterForNavigation<ViewA>("ViewA");
+                }
+        }
+}
